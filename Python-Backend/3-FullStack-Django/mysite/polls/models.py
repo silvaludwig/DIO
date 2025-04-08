a8098cbd5a349000a3360d8d1c32d5cbcc0ba3ba
Ludwig S <silvaludwig@outlook.com>
2025-04-08 10:44:17 -0300

    views e templates no Django

--- Python-Backend/3-FullStack-Django/mysite/polls/models.py
+++ Python-Backend/3-FullStack-Django/mysite/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, timezone
 
 
 class Question(models.Model):
@@ -6,8 +7,28 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     active = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Option"
+        verbose_name_plural = "Options"
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    def __str__(self):
+        return self.choice_text
