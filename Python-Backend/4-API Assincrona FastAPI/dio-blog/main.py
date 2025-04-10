faa705b65030747cc2982424e5cbf132968c0c6c
Ludwig S <silvaludwig@outlook.com>
2025-04-10 12:26:46 -0300

    rotas e endpoints fastAPI - path parameter

--- Python-Backend/4-API Assincrona FastAPI/dio-blog/main.py
+++ Python-Backend/4-API Assincrona FastAPI/dio-blog/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from datetime import datetime, UTC
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def read_root():
-    return {"message": "Hello World"}
+@app.get('/posts/{framework}')
+def read_posts(framework: str):
+    return {
+        "posts": [
+            {'title': f'Creating an application using {framework}', 'date': datetime.now(UTC)},
+            {'title': f'Internationalizing an application using {framework}', 'date': datetime.now(UTC)},
+        ]
+    }
