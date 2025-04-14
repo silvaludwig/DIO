e9c97ed7f2e1d55b992c0d37e48cbd97e0a40748
Ludwig S <silvaludwig@outlook.com>
2025-04-13 22:36:54 -0300

    organização pastas pra preparar os testes e fazendo autenticação

--- Python-Backend/4-API Assincrona FastAPI/dio-blog/schemas/post.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import UTC, datetime
-
-
-class PostIn(BaseModel):
-    title: str
-    content: str
-    published_at: datetime | None = None
-    published: bool = False
--- Python-Backend/4-API Assincrona FastAPI/dio-blog/src/models/post.py
+++ Python-Backend/4-API Assincrona FastAPI/dio-blog/src/models/post.py
@@ -1,5 +1,6 @@
-import sqlalchemy as sa 
-from database import metadata
+import sqlalchemy as sa
+
+from src.database import metadata
 
 posts = sa.Table(
     "posts",
@@ -7,6 +8,6 @@ posts = sa.Table(
     sa.Column("id", sa.Integer, primary_key=True),
     sa.Column("title", sa.String(150), nullable=False, unique=True),
     sa.Column("content", sa.String, nullable=False),
-    sa.Column("published_at", sa.DateTime, nullable=True),
+    sa.Column("published_at", sa.TIMESTAMP(timezone=True), nullable=True),
     sa.Column("published", sa.Boolean, default=False),
 )
